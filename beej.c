6bf994647835cc0bd73040ba13262936837ca9f5
7Prabhat <bloodandgravity@gmail.com>
2026-08-04 19:26:18 +0530

    pointer and use of nul

--- beej.c
+++ beej.c
@@ -2,15 +2,18 @@
 
 
 
+int my_strlen(char *s)
+{
+	char *j = s;
+
+	while (*j != '\0')
+		j++;
+
+	return j - s;
+}
+
 int main(void)
 {
-	int a[] = {11, 22, 33, 44, 55, 999};
-
-	int *p = &a[0];
-	while(*p != 999)
-	{
-		printf("%p\n", p);
-		printf("%d\n", *p);
-		p++;
-	}
+	printf("%d\n", my_strlen("Hello, World!"));
 }
+
